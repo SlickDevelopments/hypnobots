577cd8bc00d652d0440217a7776734473fd53174
Pôl <paul.beauduc@epitech.eu>
2020-05-15 18:26:42 +0200

    feat: creating 2 empty bots and a script launching them

---
+++ scripts/serve.js
@@ -0,0 +1,23 @@
+const { argv } = require('yargs');
+const colors = require('colors/safe');
+const { Probot } = require('probot');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+if (!argv._.length) {
+  console.error(colors.red(
+    '[poool.serve] No service specified, nothing to run.'
+  ));
+  process.exit(1);
+}
+
+process.on('SIGINT', process.exit);
+
+for(const arg of argv._) {
+  const app = require(`../bots/${arg}/index.js`);
+  const config = dotenv.parse(fs.readFileSync(process.cwd() + `/bots/${arg}/.env`));
+  for (const property in config) {
+    process.env[property] = config[property];
+   }
+  Probot.run(app);
+}
