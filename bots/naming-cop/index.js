21526a1a04cfa2f69682b224325d4cca32df86f6
Ugo Stephant <ugo@poool.fr>
2020-05-20 16:52:29 +0200

    fix: fix app not doing anything once on heroku

--- bots/naming-cop/index.js
+++ bots/naming-cop/index.js
@@ -1,8 +1,9 @@
+const { Probot } = require('probot');
 const check = require('./naming-cop');
 
-module.exports = app => {
+Probot.run(app => {
   app.on('pull_request.opened', check);
   app.on('pull_request.reopened', check);
   app.on('pull_request.edited', check);
   app.on('pull_request.synchronize', check);
-};
+});
