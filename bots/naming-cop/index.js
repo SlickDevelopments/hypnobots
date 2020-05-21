bd9fe4dddaef79025580f4eee04ec709e114c97f
Ugo Stephant <ugo@poool.fr>
2020-05-21 09:58:56 +0200

    fix: fix missing bot run script

--- bots/naming-cop/index.js
+++ bots/naming-cop/index.js
@@ -1,9 +1,8 @@
-const { Probot } = require('probot');
 const check = require('./naming-cop');
 
-Probot.run(app => {
+module.exports = app => {
   app.on('pull_request.opened', check);
   app.on('pull_request.reopened', check);
   app.on('pull_request.edited', check);
   app.on('pull_request.synchronize', check);
-});
+};
---
+++ scripts/run.js
@@ -0,0 +1,4 @@
+const { Probot } = require('probot');
+
+const app = require(`./bots/${process.env.BOT_NAME}`);
+Probot.run(app);
