2d0a3d46f32e5611bb0bcfaeec70544cd76e078b
Pôl <paul.beauduc@epitech.eu>
2020-05-22 12:00:59 +0200

    test(auto-review): adding basics tests for auto-review

---
+++ tests/auto-review/PullRequestReview.test.js
@@ -0,0 +1,102 @@
+const { promises: fsp } = require('fs');
+const path = require('path');
+const nock = require('nock');
+const { createProbot } = require('probot');
+
+const namingCop = require('../../bots/auto-review');
+const payload = require('../fixtures/pull_request.opened');
+
+const fixturesDir = path.resolve('./tests/fixtures');
+
+describe('Auto Review', () => {
+  let probot, cert, pullCreatedBody;
+
+  beforeAll(async () => {
+    cert = await fsp.readFile(path.join(fixturesDir, 'mock-cert.pem'));
+    pullCreatedBody = {
+      body: await fsp
+        .readFile(path.join(fixturesDir, 'branch-message.txt'), 'utf-8'),
+    };
+  });
+
+  beforeEach(async () => {
+    nock.disableNetConnect();
+    probot = createProbot({ id: 123, cert });
+    probot.load(namingCop);
+  });
+
+  test('should send a pull request review request', async () => {
+
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' });
+
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/collaborators')
+      .reply(200, {
+        data: [
+          {
+            login: 'hiimbex',
+            permissions: {
+              admin: true,
+            },
+          },
+          {
+            login: 'bexhiim',
+            permissions: {
+              admin: true,
+            },
+          }],
+      });
+    
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/contents/CODEOWNERS')
+      .reply(200, {
+        data: {
+          content: 'KiAgICAgICAgICAgICAgICAgQGhpaW1iZXg=',
+        },
+      });
+    
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/pulls/1/requested_reviewers', b => {
+        expect(b).toMatchObject({ reviewers: ['bexhiim'] });
+        return true;
+      })
+      .reply(201);
+
+    // Receive a webhook event
+    await probot.receive({ event: 'pull_request', payload });
+  });
+
+  test('should create a comment about failing', async () => {
+
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' });
+
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/collaborators')
+      .reply(200, { data: ['hiimbex'] });
+    
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/contents/CODEOWNERS')
+      .reply(404);
+
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/pull/1/comments', body => {
+        expect(body).toMatchObject({ body: 'I failed to find a reviewer :(' });
+        return true;
+      })
+      .reply(200);
+    
+    // Receive a webhook event
+    await probot.receive({ event: 'pull_request', payload });
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
