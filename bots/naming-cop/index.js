d608d87fdbaac98c483aab06756ee2823ac8e587
Pôl <paul.beauduc@epitech.eu>
2020-05-28 14:12:04 +0200

    feat(naming-cop): user can now disable naming-cop at will

--- bots/naming-cop/index.js
+++ bots/naming-cop/index.js
@@ -5,4 +5,19 @@ module.exports = app => {
   app.on('pull_request.reopened', check);
   app.on('pull_request.edited', check);
   app.on('pull_request.synchronize', check);
+
+  app.on('issue_comment.created', async context => {
+    const payload = context.payload;
+    let comment = null;
+
+    if (payload.comment.body === '@naming-cop stfu') {
+      comment = context.issue({ body: 'Okay i will now shut up.' });
+    } else if (payload.comment.body === '@naming-cop activate') {
+      comment = context.issue({ body: 'Yay i can speak.' });
+    }
+
+    if (comment !== null) {
+      context.github.issues.createComment(comment);
+    }
+  });
 };
