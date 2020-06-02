363e7e674f473c6aa2a625664d7ffdd624ef2899
Pôl <paul.beauduc@epitech.eu>
2020-06-02 16:05:00 +0200

    feat(auto-update): check if head is behind base and update it only under this condition

--- bots/auto-update/index.js
+++ bots/auto-update/index.js
@@ -6,9 +6,15 @@ module.exports = app => {
   app.on(['pull_request.opened', 'pull_request.reopened'], async context => {
     try {
       const { owner, repo, number } = context.issue();
-      const args = { owner: owner, repo: repo, pull_number: number };
+      const pull = { owner: owner, repo: repo, pull_number: number };
+      const head = context.payload.pull_request.head.label;
+      const base = context.payload.pull_request.base.label;
+      const compare = { owner: owner, repo: repo, base: base, head: head };
+      const { data } = await context.github.repos.compareCommits(compare);
       
-      await context.github.pulls.updateBranch(args);
+      if (data.behind_by > 0) {
+        await context.github.pulls.updateBranch(pull);
+      }
     } catch (e) {
       const response = `⚠️ Cannot update branch : ${e.message}`;
       const comment = context.issue({ body: response });
