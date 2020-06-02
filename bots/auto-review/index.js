3d83079da5f0bd71bf8fae96357811f7ef990fbd
Pôl <paul.beauduc@epitech.eu>
2020-06-02 15:59:32 +0200

    feat(auto-review): now check a config file to change the max number of reviewers allowed

--- bots/auto-review/index.js
+++ bots/auto-review/index.js
@@ -1,4 +1,5 @@
 const file = require('./file');
+const getConfig = require('../../utils/getConfig');
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -9,15 +10,25 @@ module.exports = app => {
     const repo = context.repo();
     const { data } = await context.github.repos.listCollaborators(repo);
     const reviewers = [];
+    const config = await getConfig(context, 'autoReview');
+    let maxAssignees = 1;
+
+    if (config && config.maxAssignees && config.maxAssignees > 0) {
+      maxAssignees = config.maxAssignees;
+    }
 
     for (const c of data) {
-      if (c.permissions.admin === true &&
-          pull.owner.toLowerCase() !== c.login.toLowerCase()) {
+      if (
+        c.permissions.admin === true &&
+        pull.owner.toLowerCase() !== c.login.toLowerCase() &&
+        !reviewers.includes(c.login.toLowerCase()) &&
+        reviewers.length < maxAssignees
+      ) {
         reviewers.push(c.login.toLowerCase());
       }
     }
     
-    await file(context, pull, 'CODEOWNERS', reviewers, data);
+    await file(context, 'CODEOWNERS', reviewers, data, maxAssignees);
     
     if (reviewers.length > 0) {
       const resquest = {
